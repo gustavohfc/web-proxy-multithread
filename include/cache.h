8a1f79f1380879f4c4bbfac1499896d2fd194657
FabioTS <ftrevizolos@gmail.com>
2017-12-03 02:18:13 -0200

    salvando cache

--- include/cache.h
+++ include/cache.h
@@ -2,7 +2,15 @@
 #ifndef CACHE_H
 #define CACHE_H
 
+#include <functional>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+
 #include "connection.h"
+
+#define CACHE_PATH /cache
 
 void getResponseMessage(Connection& connection);
 
--- src/cache.cpp
+++ src/cache.cpp
@@ -4,9 +4,18 @@
 
 void getResponseMessage(Connection& connection)
 {
-    // TODO: lock file
+    /******************************************************************************/
     // TODO: consult cache
+    // GET url from connection
+    // calculate hash
+    // search for it
+    // IF found THEN verify ELSE request
 
+    /******************************************************************************/
+	// TODO: verify valid cache
+	// IF cache is valid THEN return cache ELSE request
+
+    /******************************************************************************/
     // Request message from external server
     connection.server_socket = connectToHost(connection.client_request.getHost(), connection.status);
 
@@ -15,5 +24,27 @@ void getResponseMessage(Connection& connection)
 
     connection.receiveServerResponse();
 
-    // TODO: unlock file
+    /******************************************************************************/
+    // TODO: save new cache
+    // calculate hash and save for other requests
+    // connection.response é onde a mensagem sera retornada de acordo com a funçao "receiveMessage" em "server.cpp"
+    // usar o HTTPMessage.path como endereço completo
+
+    std::stringstream ss;
+    std::string filename;
+    std::hash<std::string> hasher;
+
+    ss << hasher(connection.response.getPath());
+    ss >> filename;
+
+    FILE* fp;
+    if((fp = fopen(filename.c_str(), "wb")) == NULL) {
+    	printf("*ERRO: Can't open file (%s)\n", filename.c_str());
+    	exit(1);
+    }
+
+    std::vector<char> data;
+    data = connection.response.getMessage();
+    fwrite(&data[0], sizeof(char), data.size(), fp);
+    fclose(fp);
 }
